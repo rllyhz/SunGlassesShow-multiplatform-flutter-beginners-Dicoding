e01acbd421faedc1964e8bb27b18b5398efc41db
rllyhz <rullyihza00@gmail.com>
2021-06-14 15:28:38 +0700

    Creating constants.dart file

---
+++ lib/constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const secondaryColor = Color(0xFFFE6D8E);
+const backgroundColor = Color(0xFFF8F8F8F8);
+
+const textColorActive = Color(0xFF12153D);
+const textColorInActive = Color.fromARGB(0x72, 0x00, 0x00, 0x00);
+
+const menuIconPath = 'assets/icons/menu.svg';
+const searchIconPath = 'assets/icons/search.svg';
+
+const paddingSm = 12.0;
+const paddingMd = 20.0;
+const paddingXl = 24.0;
+
+const titleTextSize = 24.0;
+const bodyTextSize = 20.0;
+const captionTextSize = 16.0;
+const defaultTextSize = 12.0;
+
+const cardShadow =
+    BoxShadow(offset: Offset(0, 4), blurRadius: 4, color: Colors.black26);
