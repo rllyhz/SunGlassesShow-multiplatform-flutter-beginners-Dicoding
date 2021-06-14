433c7a3ead0e17b8b05b227f7a623f91204ee75c
rllyhz <rullyihza00@gmail.com>
2021-06-14 20:00:52 +0700

    Updating home - body.dart file

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sunglasses_show/model/movie.dart';
+import 'package:sunglasses_show/model/tv_shows.dart';
 import '../../../constants.dart';
 import 'item_category_list.dart';
 
@@ -21,6 +23,9 @@ class Body extends StatelessWidget {
               child: PageView(
                 scrollDirection: Axis.horizontal,
                 controller: controller,
+                onPageChanged: (selectedIndex) {
+                  //
+                },
                 physics: BouncingScrollPhysics(),
                 children: <Widget>[
                   buildMoviesLayout(),
@@ -39,20 +44,20 @@ class Body extends StatelessWidget {
         if (constraints.maxWidth < 600) {
           return GridView.count(
             crossAxisCount: 2,
-            childAspectRatio: (2 / 3),
-            children: _generateCardList(context, 12.0, buildMoviesPreview()),
+            childAspectRatio: itemCardRatio,
+            children: _generateMovieCardList(context, itemRadiusSm),
           );
         } else if (constraints.maxWidth < 900) {
           return GridView.count(
             crossAxisCount: 3,
-            childAspectRatio: (2 / 3),
-            children: _generateCardList(context, 16.0, buildMoviesPreview()),
+            childAspectRatio: itemCardRatio,
+            children: _generateMovieCardList(context, itemRadiusMd),
           );
         } else {
           return GridView.count(
             crossAxisCount: 4,
-            childAspectRatio: (2 / 3),
-            children: _generateCardList(context, 18.0, buildMoviesPreview()),
+            childAspectRatio: itemCardRatio,
+            children: _generateMovieCardList(context, itemRadiusXl),
           );
         }
       });
@@ -62,50 +67,57 @@ class Body extends StatelessWidget {
         if (constraints.maxWidth < 600) {
           return GridView.count(
             crossAxisCount: 2,
-            childAspectRatio: (2 / 3),
-            children: _generateCardList(context, 12.0, buildTVShowsPreview()),
+            childAspectRatio: itemCardRatio,
+            children: _generateTVShowCardList(context, itemRadiusSm),
           );
         } else if (constraints.maxWidth < 900) {
           return GridView.count(
             crossAxisCount: 3,
-            childAspectRatio: (2 / 3),
-            children: _generateCardList(context, 16.0, buildTVShowsPreview()),
+            childAspectRatio: itemCardRatio,
+            children: _generateTVShowCardList(context, itemRadiusMd),
           );
         } else {
           return GridView.count(
             crossAxisCount: 4,
-            childAspectRatio: (2 / 3),
-            children: _generateCardList(context, 18.0, buildTVShowsPreview()),
+            childAspectRatio: itemCardRatio,
+            children: _generateTVShowCardList(context, itemRadiusXl),
           );
         }
       });
 
-  List<Widget> _generateCardList(
-          BuildContext ctx, double radiusSize, Image imagePreview) =>
-      List<Widget>.generate(20, (index) {
-        return InkWell(
-          onTap: () {},
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(radiusSize),
-            ),
-            elevation: 2,
-            shadowColor: Colors.black38,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(radiusSize),
-              child: imagePreview,
-            ),
-          ),
-        );
-      });
+  List<Widget> _generateMovieCardList(BuildContext ctx, double radiusSize) =>
+      movieList.map((movie) {
+        return buildCardItem(radiusSize, movie.posterUrl, () {
+          print('you clicked: ' + movie.title);
+        });
+      }).toList();
 
-  Image buildMoviesPreview() => Image.asset(
-        'assets/images/poster_overlord.jpg',
-        fit: BoxFit.fill,
+  List<Widget> _generateTVShowCardList(BuildContext ctx, double radiusSize) =>
+      tvShowList.map((tvShow) {
+        return buildCardItem(radiusSize, tvShow.posterUrl, () {
+          print('you clicked: ' + tvShow.title);
+        });
+      }).toList();
+
+  InkWell buildCardItem(
+          double radiusSize, String posterUrl, GestureTapCallback onTap) =>
+      InkWell(
+        onTap: onTap,
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(radiusSize),
+          ),
+          elevation: 2,
+          shadowColor: Colors.black26,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(radiusSize),
+            child: buildImagePreview(posterUrl),
+          ),
+        ),
       );
 
-  Image buildTVShowsPreview() => Image.asset(
-        'assets/images/poster_arrow.jpg',
+  Image buildImagePreview(String posterUrl) => Image.asset(
+        posterUrl,
         fit: BoxFit.fill,
       );
 }
