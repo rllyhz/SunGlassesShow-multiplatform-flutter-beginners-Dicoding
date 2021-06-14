17166a4300881642496f845357e9e58587a0af81
rllyhz <rullyihza00@gmail.com>
2021-06-14 21:38:01 +0700

    Updating movie.dart and tv_show.dart model files

--- lib/model/movie.dart
+++ lib/model/movie.dart
@@ -6,11 +6,3 @@ class Movie {
 
   Movie(this.title, this.posterUrl, this.year, this.rating);
 }
-
-var movieList = [
-  Movie("Overlord", "assets/images/poster_overlord.jpg", 2017, 4.6),
-  Movie("Overlord", "assets/images/poster_overlord.jpg", 2017, 5.2),
-  Movie("Overlord", "assets/images/poster_overlord.jpg", 2017, 7.5),
-  Movie("Overlord", "assets/images/poster_overlord.jpg", 2017, 5.6),
-  Movie("Overlord", "assets/images/poster_overlord.jpg", 2017, 7.2)
-];
