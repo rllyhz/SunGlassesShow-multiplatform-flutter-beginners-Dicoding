8b15e995e06d480e1aa70ee46120be57250e85b0
rllyhz <rullyihza00@gmail.com>
2021-06-14 19:59:41 +0700

    Updating constants.dart file

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,6 +7,10 @@ const textColorActive = Color(0xFF12153D);
 const textColorInActive = Color.fromARGB(0x72, 0x00, 0x00, 0x00);
 
 const itemCategories = ['Movies', 'Tv Shows'];
+const itemCardRatio = (2 / 3);
+const itemRadiusSm = 8.0;
+const itemRadiusMd = 8.0;
+const itemRadiusXl = 12.0;
 
 const menuIconPath = 'assets/icons/menu.svg';
 const searchIconPath = 'assets/icons/search.svg';
@@ -15,10 +19,7 @@ const paddingSm = 12.0;
 const paddingMd = 20.0;
 const paddingXl = 24.0;
 
-const titleTextSize = 24.0;
-const bodyTextSize = 20.0;
-const captionTextSize = 16.0;
-const defaultTextSize = 12.0;
-
-const cardShadow =
-    BoxShadow(offset: Offset(0, 4), blurRadius: 4, color: Colors.black26);
+// const titleTextSize = 24.0;
+// const bodyTextSize = 20.0;
+// const captionTextSize = 16.0;
+// const defaultTextSize = 12.0;
