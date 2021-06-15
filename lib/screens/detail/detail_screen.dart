d8e3b748c0f2cf5ff3ee7aa03e83c279efafeb3d
rllyhz <rullyihza00@gmail.com>
2021-06-16 00:50:27 +0700

    Updating Detail - detail_screen.dart file

--- lib/screens/detail/detail_screen.dart
+++ lib/screens/detail/detail_screen.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
-
+import 'package:sunglasses_show/model/movie.dart';
+import 'package:sunglasses_show/model/tv_show.dart';
 import '../../constants.dart';
-import 'components/body.dart';
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
+  final Movie? movie;
+  final TVShow? tvShow;
+  final bool isContentForMovies;
+
+  const DetailScreen(
+      {Key? key, required this.isContentForMovies, this.movie, this.tvShow})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final title = isContentForMovies ? movie!.title : tvShow!.title;
+    final synopsis = isContentForMovies ? movie!.synopsis : tvShow!.synopsis;
+    final genres = isContentForMovies ? movie!.genres : tvShow!.genres;
+    final year = isContentForMovies ? movie!.year : tvShow!.year;
+    final duration = isContentForMovies ? movie!.duration : tvShow!.duration;
+    final releasedAt =
+        isContentForMovies ? movie!.releasedAt : tvShow!.releasedAt;
+    final directorOrCreator =
+        isContentForMovies ? movie!.director : tvShow!.creator;
+    final language = isContentForMovies ? movie!.language : tvShow!.language;
+    final rate = isContentForMovies ? movie!.rate : tvShow!.rate;
+
     return Scaffold(
-      body: Body(),
+      body: SingleChildScrollView(
+        child: Text("$title ($year)"),
+      ),
       backgroundColor: backgroundColor,
     );
   }
